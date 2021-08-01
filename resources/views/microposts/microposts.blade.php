094cf32ef1b851b2ccadbf694a1c39e610e2110f
KOBAYASHI HIROAKI <kobayashihiroaki@KOBAYASHInoMacBook-Air.local>
2021-08-01 18:06:00 +0900

    ログイン時のトップページ変更

---
+++ resources/views/microposts/microposts.blade.php
@@ -0,0 +1,16 @@
+<ul class="list-unstyled">
+    @foreach ($microposts as $micropost)
+        <li class="media mb-3">
+        <img class="mr-2 rounded" src="https://www.kurieisha.com/wp-content/uploads/2021/05/007.jpg" alt="" style="width:90px">
+            <div class="media-body">
+                <div>
+                    <a href="{{route('users.show',$micropost->user->id)}}"><span class="text-muted">posted at {{ $micropost->created_at }}</span></a>
+                </div>
+                <div>
+                    <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
+                </div>
+            </div>
+        </li>
+    @endforeach
+</ul>
+{{ $microposts->links('pagination::bootstrap-4') }}
