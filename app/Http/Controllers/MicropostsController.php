7802d5c969e03adb996e1787ee8de14244a05f2e
KOBAYASHI HIROAKI <kobayashihiroaki@KOBAYASHInoMacBook-Air.local>
2021-08-05 17:52:06 +0900

    タイムラインの表示

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -15,7 +15,7 @@ class MicropostsController extends Controller
         $data = [];
         if (\Auth::check()) {
             $user = \Auth::user();
-            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(5);
+            $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
             
             $data = [
                 'user' => $user,
@@ -52,4 +52,6 @@ class MicropostsController extends Controller
 
         return back();
     }
+    
+
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    @if (Auth::check())
+     @if (Auth::check())
         <div class="row">
             <aside class="col-sm-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ Auth::user()->name }}</h3>
-                    </div>
-                    <div class="card-body">
-                    <img class="mr-2 rounded" src="https://www.kurieisha.com/wp-content/uploads/2021/05/007.jpg" alt="" style="width:90px">
-                    </div>
-                </div>
+            @include('users.card', ['user' => Auth::user()])
             </aside>
             <div class="col-sm-8">
-                @if (count($microposts) > 0)
-                    @include('microposts.microposts', ['microposts' => $microposts])
-                @endif
+    @if (Auth::id() == $user->id)
+            <form class="mb-4" method="post" action="{{ route('microposts.store') }}">
+            @csrf
+              <div class="form-group">
+                <label for="exampleFormControlTextarea1">ツイートする</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name='content'></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">投稿する</button>
+            </form>
+    @endif
+
+      @if (count($microposts) > 0)
+       @include('microposts.microposts', ['microposts' => $microposts])
+     @endif
             </div>
         </div>
     @else
